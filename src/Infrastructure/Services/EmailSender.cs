75ca75b683746e2a700d24261a4fdfc8dd8f42d8
Eric Fleming <eric-fleming18@hotmail.com>
2019-01-03 21:43:15 -0500

    Implementing email logic
    - smtp4dev
    - send grid

--- src/Infrastructure/Services/EmailSender.cs
+++ src/Infrastructure/Services/EmailSender.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.eShopWeb.ApplicationCore.Interfaces;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Net.Mail;
 using System.Threading.Tasks;
 
 namespace Microsoft.eShopWeb.Infrastructure.Services
@@ -7,10 +10,35 @@ namespace Microsoft.eShopWeb.Infrastructure.Services
     // For more details see https://go.microsoft.com/fwlink/?LinkID=532713
     public class EmailSender : IEmailSender
     {
-        public Task SendEmailAsync(string email, string subject, string message)
+        public async Task SendEmailAsync(string email, string subject, string message)
         {
-            // TODO: Wire this up to actual email sending logic via SendGrid, local SMTP, etc.
-            return Task.CompletedTask;
+            var fromEmailAddress = "eShopOnWeb@demo.com";
+            //Local SMTP
+            MailMessage mail = new MailMessage
+            {
+                From = new MailAddress(fromEmailAddress),
+                Subject = subject,
+                Body = message,
+                IsBodyHtml = true
+            };
+            mail.To.Add(email);
+
+            var smtpPort = 25;
+            SmtpClient smtp = new SmtpClient("localhost", smtpPort);
+            smtp.UseDefaultCredentials = true;
+            await smtp.SendMailAsync(mail);
+
+            //SendGrid
+            //var sendGridClient = new SendGridClient("replace-me-with-personal-sendgrid-api-key");
+            //var sendGridMessage = new SendGridMessage
+            //{
+            //    From = new EmailAddress(fromEmailAddress),
+            //    Subject = subject,
+            //    HtmlContent = message
+            //};
+            //sendGridMessage.AddTo(email);
+
+            //await sendGridClient.SendEmailAsync(sendGridMessage);
         }
     }
 }
